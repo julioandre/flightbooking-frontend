8dfeea40c77f2f683dcb410c7d0ac31d3b11f70c
julioandre275 <julioandre275@gmail.com>
2023-08-02 10:21:33 +0200

    Styling Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { Home } from './Components/Home/Home';
 
 function App() {
   return (
-   <div>App</div>
+    <>
+      <Navbar />
+      <Home />{' '}
+    </>
   );
 }
 
