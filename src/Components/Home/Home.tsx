af17dcc1e6724c0b08c83040580f2eac38b03a3b
julioandre275 <julioandre275@gmail.com>
2023-08-12 16:03:08 +0200

    Revert "Merge pull request #8 from julioandre/1-adding-airport-dropdown"
    This reverts commit 0d16fec6baad889233df993ee52f3a56a9c83e0a, reversing
    changes made to c8927fdd3425111a68a28452470baadfad0b0636.

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -8,34 +8,20 @@ import 'react-date-range/dist/theme/default.css';
 import { DateRange } from 'react-date-range';
 import format from 'date-fns/format';
 import FlightLandIcon from '@mui/icons-material/FlightLand';
-import { useEffect, useState } from 'react';
-import Autocomplete from '@mui/material/Autocomplete';
+import { useState } from 'react';
 import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 import OutlinedInput from '@mui/material/OutlinedInput';
-import { Box, Button, Grid, InputAdornment, Paper, TextField } from '@mui/material';
+import { Button, Grid, InputAdornment, Paper } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import customTheme from '../../Config/customTheme';
-import { IAirportResponse } from '../../schemas/airportResponse';
-import _ from 'lodash';
 
 export const Home = () => {
   interface selectedDestination {
     departure: string;
     arrival: string;
   }
-
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [airportList, setAirportList] = useState<IAirportResponse[]>([
-    {
-      name: "Chicago O'Hare International Airport",
-      iata_code: 'ORD',
-      icao_code: 'KORD',
-      lat: 41.978367,
-      lng: -87.904712,
-      country_code: 'US',
-    },
-  ]);
   const [destinations, setDestinations] = useState<selectedDestination>({ departure: '', arrival: '' });
   const [open, setOpen] = useState(false);
   const selectionRange = {
@@ -50,7 +36,6 @@ export const Home = () => {
     });
   };
   const handleDestinationsSwap = () => {
-    console.log(airportList);
     const temp_dest: selectedDestination = {
       departure: destinations.arrival,
       arrival: destinations.departure,
@@ -63,17 +48,6 @@ export const Home = () => {
     setStartDate(ranges.selection.startDate);
     setEndDate(ranges.selection.endDate);
   };
-  useEffect(() => {
-    const getAiports = async () => {
-      fetch(`https://airlabs.co/api/v9/airports?api_key=${process.env.REACT_APP_AIRPORT}`)
-        .then((response) => response.json())
-        .then((data) => {
-          setAirportList(data.response);
-          console.log(airportList);
-        });
-    };
-    getAiports().catch(console.error);
-  }, []);
 
   return (
     <ThemeProvider theme={customTheme}>
@@ -84,50 +58,27 @@ export const Home = () => {
           <span className="smallText">Our Packages</span>
           <h1 className="homeTitle">Search your Holiday</h1>
           <Paper elevation={2} sx={{ position: 'relative', padding: 2, background: 'white' }} className="cardDiv">
-            <Grid container alignContent={'center'} className="destinationInput">
+            <Grid container className="destinationInput">
               <Grid item xs={12} sm={5} md={3} spacing={1}>
-                <Autocomplete
-                  id="country-select-demo"
-                  sx={{ '& fieldset': { borderRadius: 5 }, my: 1, py: 1 }}
-                  options={_.uniqWith(airportList, (arrVal, othVal) => arrVal.iata_code === othVal.iata_code)}
-                  autoHighlight
-                  getOptionLabel={(option) => option.name}
-                  renderOption={(props, option) => (
-                    <Box
-                      component="li"
-                      sx={{ '& > img': { mr: 2, flexShrink: 0 } }}
-                      {...props}
-                      key={option.icao_code + option.name}
-                    >
-                      {option.name} ({option.iata_code}) +{option.icao_code}
-                    </Box>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      fullWidth
-                      placeholder="From "
-                      name="departure"
-                      InputProps={{
-                        ...params.InputProps,
-                        style: {
-                          height: '72px',
-                        },
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <FlightTakeoffIcon className="icon" color="primary" />
-                          </InputAdornment>
-                        ),
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                      }}
-                    />
-                  )}
-                />
+                <OutlinedInput
+                  sx={{ borderRadius: 5, py: 1, my: 1 }}
+                  placeholder="From? "
+                  fullWidth
+                  onChange={handleDestinations}
+                  value={destinations.departure}
+                  type="text"
+                  name="departure"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <FlightTakeoffIcon className="icon" color="primary" />
+                    </InputAdornment>
+                  }
+                />{' '}
               </Grid>
               {
-                <Grid item container xs={1} alignItems="center" justifyContent="center" direction={'column'}>
+                <Grid item xs={1} alignItems="center" justifyContent="center">
                   <Button
-                    sx={{ alignItems: 'center' }}
+                    sx={{ marginY: 2, marginX: 2 }}
                     variant="contained"
                     color="primary"
                     onClick={handleDestinationsSwap}
@@ -137,53 +88,29 @@ export const Home = () => {
                 </Grid>
               }
               <Grid item xs={12} sm={5} md={3} spacing={1}>
-
-                <Autocomplete
-                  id="country-select-demo"
-                  sx={{ '& fieldset': { borderRadius: 5 }, my: 1, py: 1 }}
-                  options={_.uniqWith(airportList, (arrVal, othVal) => arrVal.iata_code === othVal.iata_code)}
-                  autoHighlight
-                  getOptionLabel={(option) => option.name}
-                  renderOption={(props, option) => (
-                    <Box
-                      component="li"
-                      sx={{ '& > img': { flexShrink: 0 } }}
-                      {...props}
-                      key={option.icao_code + option.name}
-                    >
-                      {option.name} ({option.iata_code}) +{option.icao_code}
-                    </Box>
-                  )}
-                  renderInput={(params) => (
-                    <TextField
-                      {...params}
-                      fullWidth
-                      placeholder="To? "
-                      name="arrival"
-                      InputProps={{
-                        ...params.InputProps,
-                        style: {
-                          height: '72px',
-                        },
-                        startAdornment: (
-                          <InputAdornment position="start">
-                            <FlightLandIcon className="icon" color="primary" />
-                          </InputAdornment>
-                        ),
-                        autoComplete: 'new-password', // disable autocomplete and autofill
-                      }}
-                    />
-                  )}
-                />
-              </Grid>
-              <Grid item xs={12} sm={6} md={3} spacing={1}>
                 <OutlinedInput
-                  sx={{ borderRadius: 5, py: 1, my: 2, mx: 2 }}
+                  sx={{ borderRadius: 5, my: 1, py: 1 }}
+                  fullWidth
+                  value={destinations.arrival}
+                  placeholder="To? "
+                  name="arrival"
+                  onChange={handleDestinations}
+                  type="text"
+                  endAdornment={
+                    <InputAdornment position="end">
+                      <FlightLandIcon className="icon" color="primary" />
+                    </InputAdornment>
+                  }
+                />{' '}
+              </Grid>
+              <Grid item xs={12} sm={6} md={3}>
+                <OutlinedInput
+                  sx={{ borderRadius: 5, padding: 1, margin: 1 }}
                   value={
                     format(selectionRange.startDate, 'MM/dd/yy') + '-' + format(selectionRange.endDate, 'MM/dd/yy')
                   }
                   readOnly
-                  onChange={handleSelect}
+                  onChange={handleDestinations}
                   fullWidth
                   onClick={() => setOpen(!open)}
                   className="inputBox"
--- src/schemas/airportResponse.ts
+++
@@ -1,8 +0,0 @@
-export interface IAirportResponse {
-  name: string;
-  iata_code: string;
-  icao_code: string;
-  lat: number;
-  lng: number;
-  country_code: string;
-}
