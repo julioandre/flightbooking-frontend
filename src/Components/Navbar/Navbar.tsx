2cc56e084b7462922fa3bb7e347cfcd159e4ae3f
julioandre275 <julioandre275@gmail.com>
2023-08-13 07:00:19 +0200

    Added NavBar for desktop

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,78 +1,94 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './navbar.css';
-import { MdOutlineTravelExplore } from 'react-icons/md';
-import { AiFillCloseCircle } from 'react-icons/ai';
-import { TbGridDots } from 'react-icons/tb';
-import { useState } from 'react';
+import { AppBar, Box, Button, Container, Toolbar } from '@mui/material';
+import { TravelExplore } from '@mui/icons-material';
 
 export const Navbar = () => {
+  const pages = ['Flights', 'My Tickets', 'Explore'];
+  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const [active, setActive] = useState('navBar');
-  const showNav = () => {
-    setActive('navBar activeNavbar');
+  const showNav = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorElNav(event.currentTarget);
   };
+
   const closeNav = () => {
-    setActive('navBar');
+    setAnchorElNav(null);
   };
   return (
-    <section className="navBarSection">
-      <header className="header flex">
-        <div className="logoDiv">
-          <a href="#" className="logo flex">
-            <h1>
-              <MdOutlineTravelExplore className="icon" /> Travel.
-            </h1>
-          </a>
-        </div>
-        <div className={active}>
-          <ul className="navLists flex">
-            <li className="navItem">
-              <a href="" className="navLink">
-                Home
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                Packages
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                Shop
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                About
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                Pages
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                News
-              </a>
-            </li>
-            <li className="navItem">
-              <a href="" className="navLink">
-                Contact
-              </a>
-            </li>
-            <button className="btn">
-              <a href="#">BOOK NOW</a>
-            </button>
-          </ul>
-          <div onClick={closeNav} className="closeNavbar">
-            <AiFillCloseCircle className="icon" />
-          </div>
-        </div>
-        <div onClick={showNav} className="toggleNavbar">
-          <TbGridDots className="icon" />
-        </div>
-      </header>
-    </section>
+    <AppBar position={'static'}>
+      <Container maxWidth={'xl'}>
+        <Toolbar disableGutters>
+          <TravelExplore sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((pages) => (
+              <Button key={pages} onClick={closeNav} sx={{ my: 2, color: 'white', display: 'block' }}>
+                {pages}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    // <section className="navBarSection">
+    //   <header className="header flex">
+    //     <div className="logoDiv">
+    //       <a href="#" className="logo flex">
+    //         <h1>
+    //           <MdOutlineTravelExplore className="icon" /> Travel.
+    //         </h1>
+    //       </a>
+    //     </div>
+    //     <div className={active}>
+    //       <ul className="navLists flex">
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             Home
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             Packages
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             Shop
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             About
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             Pages
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             News
+    //           </a>
+    //         </li>
+    //         <li className="navItem">
+    //           <a href="" className="navLink">
+    //             Contact
+    //           </a>
+    //         </li>
+    //         <button className="btn">
+    //           <a href="#">BOOK NOW</a>
+    //         </button>
+    //       </ul>
+    //       <div onClick={closeNav} className="closeNavbar">
+    //         <AiFillCloseCircle className="icon" />
+    //       </div>
+    //     </div>
+    //     <div onClick={showNav} className="toggleNavbar">
+    //       <TbGridDots className="icon" />
+    //     </div>
+    //   </header>
+    // </section>
   );
 };
 export default Navbar;
