7550727d9ddab1dfdcc7837b1048c4047bed3348
julioandre275 <julioandre275@gmail.com>
2023-08-13 09:41:11 +0200

    Change NavBar type on screen size change

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,31 @@
 import React, { useState } from 'react';
 import './navbar.css';
-import { AppBar, Avatar, Box, Button, Container, IconButton, Toolbar, Tooltip } from '@mui/material';
-import { TravelExplore } from '@mui/icons-material';
+import {
+  AppBar,
+  Avatar,
+  BottomNavigation,
+  BottomNavigationAction,
+  Box,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Tooltip,
+  useMediaQuery,
+} from '@mui/material';
+import { AirplaneTicket, TravelExplore } from '@mui/icons-material';
+import PersonIcon from '@mui/icons-material/Person';
 import { ThemeProvider } from '@emotion/react';
+import FlightIcon from '@mui/icons-material/Flight';
 import customTheme from '../../Config/customTheme';
+import ExploreIcon from '@mui/icons-material/Explore';
 
 export const Navbar = () => {
+  const isMobile = useMediaQuery('(max-width:650px)');
+  const isTablet = useMediaQuery('(max-width:1200px,min-width:750px)');
   const theme = customTheme;
   const pages = ['Flights', 'My Tickets', 'Explore'];
+  const [value, setValue] = React.useState(0);
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
   const [active, setActive] = useState('navBar');
@@ -20,27 +38,46 @@ export const Navbar = () => {
   };
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position={'static'} sx={{ background: theme.palette.secondary.light }}>
-        <Container maxWidth={'xl'}>
-          <Toolbar disableGutters>
-            <TravelExplore color={'primary'} sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-              {pages.map((pages) => (
-                <Button key={pages} onClick={closeNav} sx={{ my: 2, color: 'primary', display: 'block' }}>
-                  {pages}
-                </Button>
-              ))}
-            </Box>
-            <Box sx={{ flexGrow: 0 }}>
-              <Tooltip title={'Go to Profile'}>
-                <IconButton sx={{ p: 0 }}>
-                  <Avatar alt="Remy Sharp" src="" />
-                </IconButton>
-              </Tooltip>
-            </Box>
-          </Toolbar>
-        </Container>
-      </AppBar>
+      {isMobile || isTablet ? (
+        <>
+          <BottomNavigation
+            showLabels
+            value={value}
+            onChange={(event, newValue) => {
+              setValue(newValue);
+            }}
+          >
+            <BottomNavigationAction label="Flights" icon={<FlightIcon />} />
+            <BottomNavigationAction label="Tickets" icon={<AirplaneTicket />} />
+            <BottomNavigationAction label="Explore" icon={<ExploreIcon />} />
+            <BottomNavigationAction label="Explore" icon={<PersonIcon />} />
+          </BottomNavigation>{' '}
+        </>
+      ) : (
+        <>
+          <AppBar position={'static'} sx={{ background: theme.palette.secondary.light }}>
+            <Container maxWidth={'xl'}>
+              <Toolbar disableGutters>
+                <TravelExplore color={'primary'} sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  {pages.map((pages) => (
+                    <Button key={pages} onClick={closeNav} sx={{ my: 2, color: 'primary', display: 'block' }}>
+                      {pages}
+                    </Button>
+                  ))}
+                </Box>
+                <Box sx={{ flexGrow: 0 }}>
+                  <Tooltip title={'Go to Profile'}>
+                    <IconButton sx={{ p: 0 }}>
+                      <Avatar alt="Remy Sharp" src="" />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              </Toolbar>
+            </Container>
+          </AppBar>
+        </>
+      )}
     </ThemeProvider>
     // <section className="navBarSection">
     //   <header className="header flex">
