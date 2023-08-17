4151ad8c1bee0a3b5e27316f2a83372b3a702ced
julioandre275 <julioandre275@gmail.com>
2023-08-17 21:17:55 +0200

    Added loading helper text when fetching airport list from API

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,21 +2,18 @@ import './home.css';
 import Waterfall from '../../Assets/collos.MOV';
 import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
 import { HiFilter } from 'react-icons/hi';
-import SwapHorizIcon from '@mui/icons-material/SwapHoriz';
 import 'react-date-range/dist/styles.css'; // main style file
 import 'react-date-range/dist/theme/default.css';
-import { DateRange } from 'react-date-range';
-import format from 'date-fns/format';
 import FlightLandIcon from '@mui/icons-material/FlightLand';
 import { useEffect, useState } from 'react';
 import Autocomplete from '@mui/material/Autocomplete';
-import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import { Box, Button, Grid, InputAdornment, Paper, TextField, useMediaQuery } from '@mui/material';
+import { Box, Grid, InputAdornment, Paper, TextField, useMediaQuery } from '@mui/material';
 import { ThemeProvider } from '@emotion/react';
 import customTheme from '../../Config/customTheme';
 import { IAirportResponse } from '../../schemas/airportResponse';
 import _ from 'lodash';
+import { DateRangePicker, LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 
 export const Home = () => {
   interface selectedDestination {
@@ -25,21 +22,13 @@ export const Home = () => {
   }
 
   const isMobile = useMediaQuery('(max-width:650px)');
-  const isTablet = useMediaQuery('(max-width:1200px,min-width:750px)');
+  const isTablet = useMediaQuery('(max-width:1200px),(min-width:750p)x)');
   const [startDate, setStartDate] = useState(new Date());
   const [endDate, setEndDate] = useState(new Date());
-  const [airportList, setAirportList] = useState<IAirportResponse[]>([
-    {
-      name: "Chicago O'Hare International Airport",
-      iata_code: 'ORD',
-      icao_code: 'KORD',
-      lat: 41.978367,
-      lng: -87.904712,
-      country_code: 'US',
-    },
-  ]);
+  const [airportList, setAirportList] = useState<IAirportResponse[]>([]);
   const [destinations, setDestinations] = useState<selectedDestination>({ departure: '', arrival: '' });
   const [open, setOpen] = useState(false);
+  const loading = open && airportList.length === 0;
   const selectionRange = {
     startDate: startDate,
     endDate: endDate,
@@ -71,18 +60,26 @@ export const Home = () => {
         .then((response) => response.json())
         .then((data) => {
           setAirportList(data.response);
-          console.log(airportList);
         });
     };
-    getAiports().catch(console.error);
-  }, []);
+    let active = true;
+    if (!loading) {
+      return undefined;
+    }
+    if (active) {
+      getAiports().catch(console.error);
+    }
+    return () => {
+      active = false;
+    };
+  }, [loading]);
 
   return (
     <ThemeProvider theme={customTheme}>
       <section className="home">
         <div className="overlay"></div>
         <video src={Waterfall} muted autoPlay loop></video>
-        <div className="homeContent container">
+        <Box className="homeContent container">
           <span className="smallText">Our Packages</span>
           <h1 className="homeTitle">Search your Holiday</h1>
           <Paper elevation={2} sx={{ position: 'relative', padding: 2, background: 'white' }} className="cardDiv">
@@ -90,7 +87,14 @@ export const Home = () => {
               <Grid item xs={12} sm={5} md={3} spacing={1}>
                 <Autocomplete
                   id="country-select-demo"
-                  sx={{ '& fieldset': { borderRadius: 5 }, my: 1, py: 1 }}
+                  onOpen={() => {
+                    setOpen(true);
+                  }}
+                  onClose={() => {
+                    setOpen(false);
+                  }}
+                  loading={loading}
+                  sx={{ '& fieldset': { borderRadius: 5 }, py: 1, mx: 1 }}
                   options={_.uniqWith(airportList, (arrVal, othVal) => arrVal.iata_code === othVal.iata_code)}
                   autoHighlight
                   getOptionLabel={(option) => option.name}
@@ -126,21 +130,11 @@ export const Home = () => {
                   )}
                 />
               </Grid>
-              {
-                <Grid item container xs={1.5} alignItems="center" justifyContent="center" direction={'column'}>
-                  <Button
-                    sx={{ alignItems: 'center', maxWidth: '100%' }}
-                    variant="customButton"
-                    onClick={handleDestinationsSwap}
-                  >
-                    <SwapHorizIcon fontSize="large" sx={{ color: 'white' }} />
-                  </Button>
-                </Grid>
-              }
+
               <Grid item xs={12} sm={5} md={3} spacing={1}>
                 <Autocomplete
                   id="country-select-demo"
-                  sx={{ '& fieldset': { borderRadius: 5 }, my: 1, py: 1 }}
+                  sx={{ '& fieldset': { borderRadius: 5 }, py: 1, mx: 1 }}
                   options={_.uniqWith(airportList, (arrVal, othVal) => arrVal.iata_code === othVal.iata_code)}
                   autoHighlight
                   getOptionLabel={(option) => option.name}
@@ -176,33 +170,13 @@ export const Home = () => {
                   )}
                 />
               </Grid>
-              <Grid item xs={12} sm={6} md={3} spacing={1}>
-                <OutlinedInput
-                  sx={{ borderRadius: 5, py: 1, my: 2, mx: 2 }}
-                  value={
-                    format(selectionRange.startDate, 'MM/dd/yy') + '-' + format(selectionRange.endDate, 'MM/dd/yy')
-                  }
-                  readOnly
-                  onChange={handleSelect}
-                  fullWidth
-                  onClick={() => setOpen(!open)}
-                  className="inputBox"
-                  endAdornment={
-                    <InputAdornment position="end" sx={{ pr: 1 }}>
-                      <CalendarMonthIcon className="icon" color="primary" />
-                    </InputAdornment>
-                  }
-                />
-                <div style={{ padding: 2, margin: 2 }}>
-                  {open && (
-                    <DateRange
-                      minDate={new Date()}
-                      ranges={[selectionRange]}
-                      rangeColors={['#3d91ff']}
-                      onChange={handleSelect}
-                    />
-                  )}
-                </div>
+              <Grid item container xs={12} sm={5} md={3} spacing={1} justifyContent="center" alignItems="center">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateRangePicker
+                    slotProps={{ textField: { size: 'medium' } }}
+                    sx={{ borderRadius: 5, height: '72px' }}
+                  />
+                </LocalizationProvider>
               </Grid>
             </Grid>
 
@@ -210,7 +184,7 @@ export const Home = () => {
               <HiFilter />
             </div>
           </Paper>
-        </div>
+        </Box>
       </section>
     </ThemeProvider>
   );
